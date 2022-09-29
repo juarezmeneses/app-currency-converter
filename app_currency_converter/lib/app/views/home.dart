540062affe32870179e329c2a1d0dcb0d0f7040b
juarezmeneses <juarezlmfilho@gmail.com>
2022-09-28 22:42:38 -0300

    View

---
+++ app_currency_converter/lib/app/views/home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            Image.asset('assets/logo.png'),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                children: [
+                  Expanded(
+                      child: DropdownButton<DropdownMenuItem>(
+                    items: [],
+                    onChanged: (value) {},
+                  )),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              child: Text('Convert'),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
